0ab4ab6420bf2f7cb642f97c964f0bded7b1642a
banks18 <levibanks@me.com>
2016-04-03 13:26:55 -0700

    score guide added

--- app/src/main/java/edu/up/cs301/farkle/FarkleMainActivity.java
+++ app/src/main/java/edu/up/cs301/farkle/FarkleMainActivity.java
@@ -1,10 +1,15 @@
 package edu.up.cs301.farkle;
 
+import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 
 import edu.up.cs301.game.GameMainActivity;
 import edu.up.cs301.game.GamePlayer;
 import edu.up.cs301.game.LocalGame;
+import edu.up.cs301.game.R;
 import edu.up.cs301.game.config.GameConfig;
 import edu.up.cs301.game.config.GamePlayerType;
 
@@ -67,5 +72,35 @@ public class FarkleMainActivity extends GameMainActivity {
 	@Override
 	public LocalGame createLocalGame() {
 		return new FarkleLocalGame();
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		int id = item.getItemId();
+
+		if (id == R.id.score_guide) {
+			Toast score_guide = Toast.makeText(getApplicationContext(), "5’s = 50 points\n" +
+					"1’s = 100 points\n" +
+					"1,1,1 = 300 points\n" +
+					"2,2,2 = 200 points\n" +
+					"3,3,3 = 300 points\n" +
+					"4,4,4 = 400 points\n" +
+					"5,5,5 = 500 points\n" +
+					"6,6,6 = 600 points\n" +
+					"Four of a Kind = 1,000 points\n" +
+					"Five of a Kind = 2,000 points\n" +
+					"Six of a Kind = 3,000 points\n" +
+					"A Straight of 1-6 = 1,500 points\n" +
+					"Three Pairs = 1,500 points\n" +
+					"Four of a Kind + a Pair = 1,500\n" +
+					"Two sets of Three of a Kind = 2,500", Toast.LENGTH_LONG);
+			score_guide.setGravity(Gravity.TOP|Gravity.LEFT, 800, 400);
+
+			score_guide.show();
+
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 }
