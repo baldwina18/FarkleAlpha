cb32653f7d044eaeea5db3d0b4008f9ca0e89adf
saraperkins <perkinss18@up.edu>
2016-04-08 12:39:31 -0700

    picture changer

--- app/src/main/java/edu/up/cs301/farkle/FarkleHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/farkle/FarkleHumanPlayer.java
@@ -199,8 +199,225 @@ public class FarkleHumanPlayer extends GameHumanPlayer implements View.OnClickLi
         p0scoreText.setText(myState.getPlayerScores()[0]+"");
         p1scoreText.setText(myState.getPlayerScores()[1]+"");
         runningTotalText.setText(myState.getRunningTotal()+"");
-        playerOneText.setText(this.allPlayerNames[0]);
-        playerTwoText.setText(this.allPlayerNames[1]);
+        package edu.up.cs301.farkle;
+        
+        import android.graphics.drawable.Drawable;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.ImageButton;
+        import android.widget.ImageView;
+        import android.widget.TextView;
+        
+        import org.w3c.dom.Text;
+        
+        import edu.up.cs301.game.GameHumanPlayer;
+        import edu.up.cs301.game.GameMainActivity;
+        import edu.up.cs301.game.actionMsg.GameAction;
+        import edu.up.cs301.game.infoMsg.GameInfo;
+        import edu.up.cs301.game.R;
+        
+        /**
+         * represents a human player in the game, allowing the person to interact with the
+         * gui in order to play
+         *
+         * @author Alexa Baldwin
+         * @author Levi Banks
+         * @author Sara Perkins
+         * @author Briahna Santillana
+         * @version 22 March 2016
+         */
+        public class FarkleHumanPlayer extends GameHumanPlayer implements View.OnClickListener {
+            /* ---=== Instance Variables ===--- */
+            // text views
+            protected TextView p0scoreText, p1scoreText, runningTotalText;
+            
+            // image views
+            protected static ImageView playerOneImage, playerTwoImage, farkleImage1, farkleImage2;
+            
+            // buttons
+            protected Button rollDiceButton, bankPointsButton;
+            protected ImageButton[] diceButtons = new ImageButton[6];
+            
+            //images
+            //    protected int[] picId = {R.drawable.avatar_girl, R.drawable.avatar_boy1,R.drawable.avatar_boy2,
+            //                             R.drawable.avatar_puppy};
+            
+            // image res id's
+            protected int[] diceWhiteResID = {R.drawable.white_one_die,R.drawable.white_two_die,
+            R.drawable.white_three_die, R.drawable.white_four_die,
+            R.drawable.white_five_die,R.drawable.white_six_die};
+            protected int[] diceRedResID = {R.drawable.red_one_die,R.drawable.red_two_die,
+            R.drawable.red_three_die, R.drawable.red_four_die,
+            R.drawable.red_five_die,R.drawable.red_six_die};
+            protected int[] imageButtonId = {R.id.dieOne, R.id.dieTwo, R.id.dieThree, R.id.dieFour,
+            R.id.dieFive, R.id.dieSix};
+            
+            // game play variables
+            private GameMainActivity myActivity;
+            private FarkleState myState;
+            
+            /**
+             * constructor for a human player
+             *
+             *
+             * @param name name of the player
+             */
+            public FarkleHumanPlayer(String name) {
+                super(name);
+                
+                
+            }
+            
+            /**
+             * Returns the GUI's top view object
+             *
+             * @return the top object in the GUI's view hierarchy
+             */
+            @Override
+            public View getTopView() {
+                return myActivity.findViewById(R.id.top_gui_layout);
+            }
+            
+            /**
+             * callback method when we get a message (e.g., from the game)
+             *
+             * @param info the message -- to be used if a game state
+             */
+            @Override
+            public void receiveInfo(GameInfo info) {
+                // ignore the message if it's not a FarkleState message
+                if (!(info instanceof FarkleState)) return;
+                
+                // update our state
+                this.myState = (FarkleState)info;
+                
+                if(myState.hasFarkle()) {
+                    farkleImage1.setVisibility(View.VISIBLE);
+                    farkleImage2.setVisibility(View.VISIBLE);
+                    
+                    
+                    
+                }
+                updateDisplay();
+            }
+            
+            /**
+             * callback method--our game has been chosen/rechosen to be the GUI,
+             * called from the GUI thread
+             * sets all of the view variables
+             *
+             * @param activity the activity under which we are running
+             */
+            public void setAsGui(GameMainActivity activity) {
+                
+                myActivity = activity;
+                activity.setContentView(R.layout.farkle_human_player);
+                
+                // text views
+                p0scoreText = (TextView)activity.findViewById(R.id.p0CurrentScore);
+                p1scoreText = (TextView)activity.findViewById(R.id.p1CurrentScore);
+                runningTotalText = (TextView)activity.findViewById(R.id.bankScore);
+                
+                // image views
+                playerOneImage = (ImageView)activity.findViewById(R.id.playerOneImage);
+                playerTwoImage = (ImageView)activity.findViewById(R.id.playerTwoImage);
+                farkleImage1 = (ImageView)activity.findViewById(R.id.farkleTextImageView1);
+                farkleImage2 = (ImageView)activity.findViewById(R.id.farkleTextImageView2);
+                
+                playerOneImage.setImageResource(R.drawable.avatar_girl);
+                playerTwoImage.setImageResource(R.drawable.avatar_puppy);
+                
+                // buttons
+                rollDiceButton = (Button)activity.findViewById(R.id.rollDiceButton);
+                bankPointsButton = (Button)activity.findViewById(R.id.bankPointsButton);
+                ImageButton dieOne = (ImageButton)activity.findViewById(R.id.dieOne);
+                ImageButton dieTwo = (ImageButton)activity.findViewById(R.id.dieTwo);
+                ImageButton dieThree = (ImageButton)activity.findViewById(R.id.dieThree);
+                ImageButton dieFour = (ImageButton)activity.findViewById(R.id.dieFour);
+                ImageButton dieFive = (ImageButton)activity.findViewById(R.id.dieFive);
+                ImageButton dieSix = (ImageButton)activity.findViewById(R.id.dieSix);
+                diceButtons[0] = dieOne;  diceButtons[1] = dieTwo;  diceButtons[2] = dieThree;
+                diceButtons[3] = dieFour; diceButtons[4] = dieFive; diceButtons[5] = dieSix;
+                
+                // listeners
+                for(ImageButton ib : diceButtons) { ib.setOnClickListener(this); }
+                rollDiceButton.setOnClickListener(this);
+                bankPointsButton.setOnClickListener(this);
+                
+            }
+            
+            /**
+             * handle all click events -- actions made by player
+             * @param button view that was clicked
+             */
+            public void onClick(View button) {
+                
+                // if we are not yet connected to a game, ignore
+                if (game == null) return;
+                int id = button.getId();
+                
+                // Construct the action and send it to the game
+                GameAction action = null;
+                
+                if (id == R.id.bankPointsButton) {
+                    action = new BankPointsAction(this);
+                }
+                else if (id == R.id.rollDiceButton) {
+                    action = new RollAction(this);
+                }
+                // Checks for all image buttons
+                else {
+                    for(int i=0; i<imageButtonId.length; i++) {
+                        if(id == imageButtonId[i]) {
+                            action = new SelectDieAction(this, i);
+                            break;
+                        }
+                    }
+                }
+                if (action != null) {
+                    game.sendAction(action); // send action to the game
+                }
+            }
+            
+            /**
+             * display the die using the image id's
+             */
+            public void displayDie() {
+                for(int i=0; i<6;i++) {
+                    Die curDie = myState.getDice()[i];
+                    if(!curDie.isInPlay()) {
+                        diceButtons[i].setVisibility(View.INVISIBLE);
+                    }
+                    else {
+                        diceButtons[i].setVisibility(View.VISIBLE);
+                        if(curDie.isSelected()) {
+                            diceButtons[i].setImageResource(diceWhiteResID[curDie.getValue()-1]);
+                        }
+                        else {
+                            diceButtons[i].setImageResource(diceRedResID[curDie.getValue()-1]);
+                        }
+                    }
+                }
+            }
+            
+            /**
+             * set the display based on the current state
+             */
+            protected void updateDisplay() {
+                //update dice
+                displayDie();
+                
+                //set scores & running total
+                p0scoreText.setText(myState.getPlayerScores()[0]+"");
+                p1scoreText.setText(myState.getPlayerScores()[1]+"");
+                runningTotalText.setText(myState.getRunningTotal()+"");
+                playerOneText.setText(this.allPlayerNames[0]);
+                playerTwoText.setText(this.allPlayerNames[1]);
+            }
+            
+            
+        }
+
     }
 
 }
--- app/src/main/java/edu/up/cs301/farkle/FarkleMainActivity.java
+++ app/src/main/java/edu/up/cs301/farkle/FarkleMainActivity.java
@@ -1,8 +1,9 @@
 package edu.up.cs301.farkle;
 
-import android.view.Gravity;
+import android.graphics.Color;
+import android.media.Image;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -23,84 +24,110 @@ import edu.up.cs301.game.config.GamePlayerType;
  * @version 15 March 2016
  */
 public class FarkleMainActivity extends GameMainActivity {
-	// for networked play
-	private static final int PORT_NUMBER = 2234;
-
-	/**
-	 * Create the default configuration for this game:
-	 * - one human player vs. one computer player
-	 * - minimum of 1 player, maximum of 2
-	 * - one kind of computer player and one kind of human player available
-	 *
-	 * @return the new configuration object, representing the default configuration
-	 */
-	@Override
-	public GameConfig createDefaultConfig() {
-
-		// Define the allowed player types
-		ArrayList<GamePlayerType> playerTypes = new ArrayList<GamePlayerType>();
-
-		// Pig has two player types:  human and computer
-		playerTypes.add(new GamePlayerType("Local Human Player") {
-			public GamePlayer createPlayer(String name) {
-				return new FarkleHumanPlayer(name);
-			}});
-		playerTypes.add(new GamePlayerType("Computer Player") {
-			public GamePlayer createPlayer(String name) {
-				return new FarkleDumbComputerPlayer(name);
-			}});
-		playerTypes.add(new GamePlayerType("Hard Computer Player") {
-			public GamePlayer createPlayer(String name) {
-				return new FarkleSmartComputerPlayer(name);
-			}});
-
-
-		// Create a game configuration class for Pig:
-		GameConfig defaultConfig = new GameConfig(playerTypes, 1, 2, "Farkle", PORT_NUMBER);
-		defaultConfig.addPlayer("Human", 0); // player 1: a human player
-		defaultConfig.addPlayer("Computer Smart", 2); // player 2: a computer player
-		defaultConfig.setRemoteData("Remote Human Player", "", 0);
-
-		return defaultConfig;
-	}
-
-	/**
-	 * create a local game
-	 *
-	 * @return the local farkle game
-	 */
-	@Override
-	public LocalGame createLocalGame() {
-		return new FarkleLocalGame();
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		int id = item.getItemId();
-
-		if (id == R.id.score_guide) {
-			Toast score_guide = Toast.makeText(getApplicationContext(), "5’s = 50 points\n" +
-					"1’s = 100 points\n" +
-					"1,1,1 = 300 points\n" +
-					"2,2,2 = 200 points\n" +
-					"3,3,3 = 300 points\n" +
-					"4,4,4 = 400 points\n" +
-					"5,5,5 = 500 points\n" +
-					"6,6,6 = 600 points\n" +
-					"Four of a Kind = 1,000 points\n" +
-					"Five of a Kind = 2,000 points\n" +
-					"Six of a Kind = 3,000 points\n" +
-					"A Straight of 1-6 = 1,500 points\n" +
-					"Three Pairs = 1,500 points\n" +
-					"Four of a Kind + a Pair = 1,500\n" +
-					"Two sets of Three of a Kind = 2,500", Toast.LENGTH_LONG);
-			score_guide.setGravity(Gravity.TOP|Gravity.LEFT, 800, 400);
-
-			score_guide.show();
-
-			return true;
-		}
-
-		return super.onOptionsItemSelected(item);
-	}
+    // for networked play
+    private static final int PORT_NUMBER = 2234;
+    protected static ImageView playerOneImage, playerTwoImage;
+    //or I can do references of some kind in GameState
+    
+    /**
+     * Create the default configuration for this game:
+     * - one human player vs. one computer player
+     * - minimum of 1 player, maximum of 2
+     * - one kind of computer player and one kind of human player available
+     *
+     * @return the new configuration object, representing the default configuration
+     */
+    @Override
+    public GameConfig createDefaultConfig() {
+        
+        // Define the allowed player types
+        ArrayList<GamePlayerType> playerTypes = new ArrayList<GamePlayerType>();
+        
+        // Farkle has two player types:  human and computer
+        playerTypes.add(new GamePlayerType("Local Human Player") {
+            public GamePlayer createPlayer(String name) {
+                return new FarkleHumanPlayer(name);
+            }});
+        playerTypes.add(new GamePlayerType("Computer Player") {
+            public GamePlayer createPlayer(String name) {
+                return new FarkleDumbComputerPlayer(name);
+            }});
+        playerTypes.add(new GamePlayerType("Hard Computer Player") {
+            public GamePlayer createPlayer(String name) {
+                return new FarkleSmartComputerPlayer(name);
+            }});
+        
+        
+        // Create a game configuration class for Farkle:
+        GameConfig defaultConfig = new GameConfig(playerTypes, 1, 2, "Farkle", PORT_NUMBER);
+        defaultConfig.addPlayer("Human", 0); // player 1: a human player
+        defaultConfig.addPlayer("Computer Smart", 2); // player 2: a computer player
+        defaultConfig.setRemoteData("Remote Human Player", "", 0);
+        
+        
+        return defaultConfig;
+    }
+    
+    /**
+     * create a local game
+     *
+     * @return the local farkle game
+     */
+    @Override
+    public LocalGame createLocalGame() {
+        return new FarkleLocalGame();
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        
+        //image views
+        playerOneImage = FarkleHumanPlayer.playerOneImage;
+        playerTwoImage = FarkleHumanPlayer.playerTwoImage;
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.girl) {
+            playerOneImage.setImageResource(R.drawable.avatar_girl);
+            return true;
+        }
+        else if (id == R.id.boyBlackHair)
+        {
+            playerOneImage.setImageResource(R.drawable.avatar_boy1);
+            return true;
+        }
+        else if (id == R.id.boyRedHair)
+        {
+            playerOneImage.setImageResource(R.drawable.avatar_boy2);
+            return true;
+        }
+        else if (id == R.id.dog)
+        {
+            playerOneImage.setImageResource(R.drawable.avatar_puppy);
+            return true;
+        }
+        else if (id == R.id.girl1) {
+            playerTwoImage.setImageResource(R.drawable.avatar_girl);
+            return true;
+        }
+        else if (id == R.id.boyBlackHair1)
+        {
+            playerTwoImage.setImageResource(R.drawable.avatar_boy1);
+            return true;
+        }
+        else if (id == R.id.boyRedHair1)
+        {
+            playerTwoImage.setImageResource(R.drawable.avatar_boy2);
+            return true;
+        }
+        else if (id == R.id.dog1)
+        {
+            playerTwoImage.setImageResource(R.drawable.avatar_puppy);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
